d5c0aa8f64cb0c26481a697a0ace67f0d76b45e6
PXTN2 <150564535+PXTN2@users.noreply.github.com>
2023-11-14 23:52:21 +0700

    .

--- routes/restaurant.router.js
+++ routes/restaurant.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getAll ,getById ,creatRes, deleteById, updateById} = require ('../controllers/restaurant.controller')
+const {getAll ,getById ,create, deleteById, updateById} = require ('../controllers/restaurant.controller')
 const {authJwt} = require("../middleware/index")
 
 
 router.get("/restaurant", getAll);
-router.get("/restaurant/:id", [authJwt.verifyToken, authJwt.isAdmin], getById)
-router.post('/restaurant',creatRes)
+router.get("/restaurant/:id",[authJwt.verifyToken, authJwt.isAdmin], getById)
+router.post('/restaurant',create)
 router.delete('/restaurant/:id',[authJwt.verifyToken, authJwt.isAdmin],deleteById)
 router.put('/restaurant/:id', [authJwt.verifyToken, authJwt.isAdmin],updateById)
 
